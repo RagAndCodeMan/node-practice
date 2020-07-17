a93ba36aaff7d0c7aea446be221a4b69de2c1b91
RagAndCodeMan <idmitriywinx@gmail.com>
2020-07-17 22:44:37 +0300

    No English

--- index.js
+++ index.js
@@ -1,21 +1,27 @@
 const express = require('express');
 const app = express()
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser')
+const Path = require("path")
+
+const PORT = process.env.PORT || 3000
 
 app.use(bodyParser.urlencoded({extended: false}))
+app.use(cookieParser('koke0f0k3finvjjif3'))
 
 app.get("/favicon.ico", (req, res) => {
-    res.sendfile("./favicon.ico")
+    res.sendFile(Path.join(__dirname, './favicon.ico'))
 })
 
 app.get("/", (req, res) => {
     res.setHeader("Content-Type", "text/html; charset=utf-8")
+
     res.end(`
         <h1>Фёдоров App</h1>
         <h3>Напиши своё имя и фамилию, и ты узнаешь, насколько ты лучше Артёма</h3>
         <form action="/create_result/" method="POST">
-            <label>Твоё Имя</label><input autocomplete="off" type="text" name="your_name" style="margin-left: 20px"/> <br /> <br />
-            <label>Твоя фамилия</label><input autocomplete="off" type="text" name="your_sirname" style="margin-left: 20px"/> <br /> <br />
+            <label>Твоё Имя</label><input title="Разрешено использование только русских символов" pattern="^[А-Яа-яЁё\s]+$" autocomplete="off" type="text" name="your_name" style="margin-left: 20px"/> <br /> <br />
+            <label>Твоя фамилия</label><input title="Разрешено использование только русских символов" pattern="^[А-Яа-яЁё\s]+$" autocomplete="off" type="text" name="your_sirname" style="margin-left: 20px"/> <br /> <br />
             <label>Любимая цитата</label><input autocomplete="off" type="text" name="your_blockquote" style="margin-left: 20px"/> <br /> <br />
             <input type="submit">
         </form>
@@ -25,6 +31,7 @@ app.get("/", (req, res) => {
 app.post("/create_result/", (req, res) => {
     res.status(200)
     res.setHeader("Content-Type", "text/html; charset=utf-8")
+
     const recievedData = req.body
     const randomPercent = Math.round(Math.random() * 100)
 
@@ -37,6 +44,6 @@ app.post("/create_result/", (req, res) => {
     }
 })
 
-app.listen(process.env.PORT, () => {
+app.listen(PORT, () => {
     console.log("The server has been listened")
 })
